028ec500977918b3b78259c7f46f1fc1e58f39ad
jlazo3010 <jorge3010@ciencias.unam.mx>
2025-06-18 12:10:44 -0600

    Update pagina_mezcal.py

--- pagina_mezcal.py
+++ pagina_mezcal.py
@@ -69,7 +69,7 @@ html, body, .main, .block-container {
 /* Aviso fijo en pantalla para dirigir al sidebar */
 .finaliza-sidebar {
     position: fixed;
-    top: 45%;
+    top: 40%;
     left: 5px;
     background-color: #004d01;
     color: white;
